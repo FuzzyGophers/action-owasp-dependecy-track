5d8a776ad58541ce902da22a765f40409497e41c
Aaron Bauman <bauman.aaron@gmail.com>
2023-05-25 15:25:52 -0400

    fix error

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,7 +56,7 @@ case $LANGUAGE in
         
         apk add --update npm
 
-        export NVM_DIR="$HOME/.nvm" && (
+        #export NVM_DIR="$HOME/.nvm" && (
         #git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
         #cd "$NVM_DIR"
         #git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
